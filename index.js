e06a0af93e58dde863541f1c3cb31786d48c1fb0
NozomiKitagawa <nonono2xxu_u@yahoo.co.jp>
2021-03-29 18:53:43 +0900

    レスポンシブで要素はみでたのでアニメーション戻す

--- index.js
+++ index.js
@@ -40,59 +40,59 @@ $(function () {
 });
 
 // 画面が読み込まれた時、is-slideを外し、アニメーションさせる
-// $(window).on('load', function () {
-//     $('body').removeClass('is-slide');
-// });
-
-// $(function () {
-// ハッシュリンク(#)と別ウィンドウでページを開く場合は実行しない
-
-//     $('a:not([href^="#"]):not([target])').on('click', function (e) {
-//         e.preventDefault();         // ページ遷移を一旦キャンセル
-//         url = $(this).attr('href'); // 遷移先のURLを取得
-
-//         if (url !== '') {
-//             $('body').addClass('is-slide-in'); // 画面遷移前のアニメーション is-slide-in
-
-//             setTimeout(function () {
-//                 window.location = url;  // 0.7秒後に取得したURLに遷移
-//             }, 700);
-//         }
-//         return false;
-//     });
-// });
-
-// 追加ローディングアニメーション
-$(function () {
-    var loader = $('.loader-wrap');
-
-    //ページの読み込みが完了したらアニメーションを非表示
-    $(window).on('load', function () {
-        loader.fadeOut();
-    });
-
-    //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
-    setTimeout(function () {
-        loader.fadeOut();
-    }, 3000);
+$(window).on('load', function () {
+    $('body').removeClass('is-slide');
 });
 
-
-// トップページへのボタン(フェイドして現れる)
 $(function () {
-    var pagetop = $('.pagetop');
-    pagetop.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
-            pagetop.fadeIn();
-        } else {
-            pagetop.fadeOut();
+    // ハッシュリンク(#)と別ウィンドウでページを開く場合は実行しない
+
+    $('a:not([href^="#"]):not([target])').on('click', function (e) {
+        e.preventDefault();         // ページ遷移を一旦キャンセル
+        url = $(this).attr('href'); // 遷移先のURLを取得
+
+        if (url !== '') {
+            $('body').addClass('is-slide-in'); // 画面遷移前のアニメーション is-slide-in
+
+            setTimeout(function () {
+                window.location = url;  // 0.7秒後に取得したURLに遷移
+            }, 700);
         }
-    });
-    pagetop.click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500); //0.5秒かけてトップへ移動
         return false;
     });
 });
+
+// 追加ローディングアニメーション
+// $(function () {
+//     var loader = $('.loader-wrap');
+
+    //ページの読み込みが完了したらアニメーションを非表示
+    // $(window).on('load', function () {
+    //     loader.fadeOut();
+    // });
+
+    //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
+//     setTimeout(function () {
+//         loader.fadeOut();
+//     }, 3000);
+// });
+
+
+// トップページへのボタン(フェイドして現れる)
+// $(function () {
+//     var pagetop = $('.pagetop');
+//     pagetop.hide();
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+//             pagetop.fadeIn();
+//         } else {
+//             pagetop.fadeOut();
+//         }
+//     });
+//     pagetop.click(function () {
+//         $('body,html').animate({
+//             scrollTop: 0
+//         }, 500); //0.5秒かけてトップへ移動
+//         return false;
+//     });
+// });
