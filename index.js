a3792a79463a94191d2374626c8835c86c983a71
NozomiKitagawa <nonono2xxu_u@yahoo.co.jp>
2021-03-29 18:12:45 +0900

    ページ遷移時のアニメーション変更

--- index.js
+++ index.js
@@ -40,27 +40,43 @@ $(function () {
 });
 
 // 画面が読み込まれた時、is-slideを外し、アニメーションさせる
-$(window).on('load', function () {
-    $('body').removeClass('is-slide');
-});
+// $(window).on('load', function () {
+//     $('body').removeClass('is-slide');
+// });
 
+// $(function () {
+// ハッシュリンク(#)と別ウィンドウでページを開く場合は実行しない
+
+//     $('a:not([href^="#"]):not([target])').on('click', function (e) {
+//         e.preventDefault();         // ページ遷移を一旦キャンセル
+//         url = $(this).attr('href'); // 遷移先のURLを取得
+
+//         if (url !== '') {
+//             $('body').addClass('is-slide-in'); // 画面遷移前のアニメーション is-slide-in
+
+//             setTimeout(function () {
+//                 window.location = url;  // 0.7秒後に取得したURLに遷移
+//             }, 700);
+//         }
+//         return false;
+//     });
+// });
+
+// 追加ローディングアニメーション
 $(function () {
-    // ハッシュリンク(#)と別ウィンドウでページを開く場合は実行しない
+    var loader = $('.loader-wrap');
 
-    $('a:not([href^="#"]):not([target])').on('click', function (e) {
-        e.preventDefault();         // ページ遷移を一旦キャンセル
-        url = $(this).attr('href'); // 遷移先のURLを取得
-
-        if (url !== '') {
-            $('body').addClass('is-slide-in'); // 画面遷移前のアニメーション is-slide-in
-
-            setTimeout(function () {
-                window.location = url;  // 0.7秒後に取得したURLに遷移
-            }, 700);
-        }
-        return false;
+    //ページの読み込みが完了したらアニメーションを非表示
+    $(window).on('load', function () {
+        loader.fadeOut();
     });
+
+    //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
+    setTimeout(function () {
+        loader.fadeOut();
+    }, 3000);
 });
+
 
 // トップページへのボタン(フェイドして現れる)
 $(function () {
